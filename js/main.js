dac2d23f1b6d814b5ce0cb052cfc083756ffa707
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-29 17:50:30 +0200

    Creato index con js e css

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+const oggetti = [
+    {
+      url: 'img/01.jpg',
+      titolo: 'Svezia',
+      descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+      url: 'img/02.jpg',
+      titolo: 'Svizzera',
+      descrizione: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+      url: 'img/03.jpg',
+      titolo: 'Gran Bretagna',
+      descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+      url: 'img/04.jpg',
+      titolo: 'Germania',
+      descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+      url: 'img/05.jpg',
+      titolo: 'Paradise',
+      descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    }
+  ];
+
+  
